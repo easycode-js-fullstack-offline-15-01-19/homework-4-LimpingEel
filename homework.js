5cfcd770b1239233c48861c86868f0ba0b144553
LimpingEel <nik.doroshenko.00@ukr.net>
2019-02-03 22:11:48 +0200

    added task #1, ... , #6

--- homework.js
+++ homework.js
@@ -1,2 +1,87 @@
 // -------------------------- Home work --------------------------
-// -------------------------- ФИО --------------------------
+// -------------------------- Дорошенко Никита Андреевич --------------------------
+
+// Функции. Задачи.
+// #1
+
+// function multiply() {
+// 	let result = 1;
+// 	for (let i = 0; i < arguments.length; i++) {
+// 		result *= arguments[i]
+// 	}
+// 	return result;
+// }
+
+// #2
+
+// function str(a) {
+// 	if (typeof a !== 'string') return console.log('Error')
+// 	let newStr = '';
+// 	for (let i = a.length - 1; i >= 0; i--) {
+// 		newStr += a[i]
+// 	}
+// 	return newStr
+// }
+
+// #3
+
+// function a(str) {
+// 	let n = '';
+// 	for (let i = 0; i < str.length; i++) {
+// 			n += str.charCodeAt(i) + ' ';
+// 	}
+// 	return n
+// }
+
+// #4                
+
+// function yourNumber(number) {
+// 	if (number < 0 || number > 10) { return console.log('Error'); }
+// 	let random = Math.round(Math.random() * 10);
+// 	if (number === random) {
+// 		console.log('Вы выиграли')
+// 	} else {
+// 		console.log(`Вы не угадали ваше число ${number}, а выпало число ${random}`)
+// 	}
+// }
+
+// #5
+
+// function a(number) {
+// 	let arr = [];
+// 	for (let i = 1; i <= number; i++) {
+// 		arr.push(i)
+// 	}
+// 	return arr
+// }
+
+// #6
+
+// let arr = [1,2,3];
+
+// function doubleArray(arr) {
+// 	arr2 = arr.concat(arr)
+// 	return arr2
+// }
+
+// #7        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+function sumArr() {
+	let result = [];
+	return result
+}
+
+// #8
+function a(users, 'gender', 'male')
+
+
+
+
+// Функции высшего порядка. Задачи.
+
+// #1
+
+function firstFunc(arr, handler1) {
+	let str = 'New value:' + firstFunc()
+}
+
